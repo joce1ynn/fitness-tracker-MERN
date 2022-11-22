5f71183a5130dd4c23df8bc49195a2a464aaee6e
Wenwen Tian <wwtian9@gmail.com>
2022-11-21 20:20:00 -0700

    edit title

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+// connect with react router
 // serve up react front-end in production
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, "../../build/index.html"));
