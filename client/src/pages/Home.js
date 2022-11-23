cb7178cafe7481df98fef7df249f15eca948d55a
Wenwen Tian <wwtian9@gmail.com>
2022-11-22 23:22:32 -0700

    add home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Your Daily Workout Buddy</h1>
+      <p>Cardio? Resistance? Or both? Track your daily exercises and stay fit with us.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
