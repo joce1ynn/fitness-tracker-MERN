e97a8ef6b28986b8b75055663276432c5da89f58
Wenwen Tian <wwtian9@gmail.com>
2022-12-27 17:30:49 -0700

    fix proxy bug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}, err => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!')
+}
+
+);
 
 module.exports = mongoose.connection;
