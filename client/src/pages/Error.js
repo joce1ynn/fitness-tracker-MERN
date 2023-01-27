2f170f410fe7e64ddf90d0fe5b403c32fb1e53a3
Wenwen Tian <wwtian9@gmail.com>
2023-01-27 11:57:20 -0700

    deploying on render

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../components/Header"
 
 export default function Error() {
-  return <div>Oops, we couldn't find that page.</div>;
+  return (
+    <div >
+      <Header />
+      <div className="d-flex align-items-center justify-content-center mt-5">
+        <h3>Oops, we couldn't find that page.</h3>
+      </div>
+    </div>
+  )
 }
--- server/server.js
+++ server/server.js
@@ -14,6 +14,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
